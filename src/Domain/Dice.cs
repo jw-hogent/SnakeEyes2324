7cb2e1b0a813f7c5cac09174bde4dd49db9f3306
Jan Willem <102214064+jw-hogent@users.noreply.github.com>
2023-10-18 11:22:42 +0200

    Implement Domain

---
+++ src/Domain/Dice.cs
@@ -0,0 +1,17 @@
+﻿namespace Domain;
+
+internal class Dice
+{
+    private readonly Random _randomizer = new();
+
+    internal int Dots { get; private set; } = 6;
+
+    internal Dice()
+    {
+    }
+
+    internal void Roll()
+    {
+        Dots = _randomizer.Next(1, 7);
+    }
+}
---
+++ src/Domain/Game.cs
@@ -0,0 +1,47 @@
+﻿namespace Domain;
+
+internal class Game
+{
+    private List<int> _highscores = new();
+    private Dice _dice1 = new();
+    private Dice _dice2 = new();
+
+    public int Eye1 => _dice1.Dots;
+    public int Eye2
+    {
+        get { return _dice2.Dots; }
+    }
+    public bool HasSnakeEyes => Eye1 == 1 && Eye2 == 1;
+    public IReadOnlyList<int> HighScores => _highscores.AsReadOnly();
+    public int Total { get; set; }
+
+    public Game()
+    {
+        Initialize();
+    }
+
+    private void Initialize()
+    {
+        _dice1 = new();
+        _dice2 = new();
+    }
+
+    public void Play()
+    {
+        _dice1.Roll();
+        _dice2.Roll();
+        if (HasSnakeEyes)
+        {
+            _highscores.Add(Total);
+            Total = 0;
+        }
+        else
+        {
+            Total += Eye1 + Eye2;
+        }
+    }
+    public void Restart()
+    {
+        Initialize();
+    }
+}
